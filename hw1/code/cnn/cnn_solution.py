3b1aab0eccccb8ef53e3a79154e60e6102e0417d
Shivanshu Shekhar <sasuke@Shivanshus-MacBook-Pro.local>
2026-02-01 14:55:13 -0600

    added val

--- hw1/code/cnn/cnn_solution.py
+++ hw1/code/cnn/cnn_solution.py
@@ -12,7 +12,7 @@ from typing import Callable, List, Tuple
 
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from torchvision import datasets, transforms
 
 import matplotlib.pyplot as plt
@@ -279,8 +279,8 @@ def make_datasets() -> Tuple[torch.utils.data.Dataset, torch.utils.data.Dataset]
         transforms.Normalize((0.5,), (0.5,)),
     ])
 
-    train_set = datasets.MNIST(root="./data", train=True, download=True, transform=transform)
-    test_set = datasets.MNIST(root="./data", train=False, download=True, transform=transform)
+    total_data = datasets.MNIST(root="./data", train=True, download=True, transform=transform)
+    train_set, test_set = random_split(total_data, [0.8, 0.2])
     return train_set, test_set
 
 
